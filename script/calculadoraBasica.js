b807e8217c2bb0e0b0fa34d93d073e3daf572e07
RomuloDsouza <0000402106@senaimgaluno.com.br>
2024-05-15 18:52:20 -0300

    Pronta

---
+++ script/calculadoraBasica.js
@@ -0,0 +1,55 @@
+function calcular(){
+    try{
+    let valor1 = document.getElementById('numero1').value;
+    let valor2 = document.getElementById('numero2').value;
+    let operacao = checkRatio();
+
+    if(/^[0-9]/.test(valor1) && /^[0-9]/.test(valor2)){
+        valor1 = parseFloat(valor1);
+        valor2 = parseFloat(valor2);
+    }else{
+        throw new Error("Digite somente números")
+    }
+
+    let result = teste(valor1, valor2, operacao);
+    result = (parseFloat(result)).toFixed('2');
+
+    let atualizar = document.getElementById('resultado');
+    atualizar.textContent = result;
+    atualizar.style.visibility = "visible";
+
+    }catch(error){
+        alert(error.message);
+    }
+}
+
+function checkRatio(){
+    let radios = document.getElementsByName('opcao')
+
+    for (let i = 0; i < radios.length; i++){
+      if(radios[i].checked){
+        return radios[i].value;
+      }
+    }
+  };
+
+function teste(valor1, valor2, operacao) {
+    switch (operacao) {
+      case '+':
+        return valor1 + valor2;
+      case '-':
+        return valor1 - valor2;
+      case '*':
+        return valor1 * valor2;
+        case '/':
+          let teste = valor1 / valor2
+          if(teste === 0 || teste == 'Infinity'){
+            return 0;
+          }
+          return valor1 / valor2;
+      default:
+        break;
+    }
+    
+    
+  }
---
+++ script/combustivel.js
@@ -0,0 +1,43 @@
+function calcular(){
+    try{
+    let valorAlcool = document.getElementById('alcool').value;
+    let valorGasolina = document.getElementById('gasolina').value;
+    let abAlcool = document.getElementById('abAlcool').value;
+    let abGasolina = document.getElementById('abGasolina').value;
+    let resultAlcool = 0;
+    let resultGasolina = 0;
+
+    if(/^[0-9]/.test(valorAlcool) && /^[0-9]/.test(valorGasolina) && /^[0-9]/.test(abAlcool) && /^[0-9]/.test(abGasolina)){
+        valorAlcool = parseFloat(valorAlcool);
+        valorGasolina = parseFloat(valorGasolina);
+        abAlcool = parseFloat(abAlcool);
+        abGasolina = parseFloat(abGasolina);
+        resultAlcool = valorAlcool * abAlcool;
+        resultGasolina = valorGasolina * abGasolina;
+    }else{
+        throw new Error("Digite somente números")
+    }
+
+    let result = document.getElementById('resultadoFinal');
+    if((valorAlcool/valorGasolina) > 0.3){
+        result = "Nessa faixa preço, da  gasolina vale mais apena"
+    }else{
+        result = "Nessa faixa preço, do  álcool vale mais apena"
+    }
+
+    let atualizar1 = document.getElementById('resultado1');
+    atualizar1.textContent = "Valor álcool: R$ " + resultAlcool.toFixed('2');
+    atualizar1.style.visibility = "visible";
+
+    let atualizar2 = document.getElementById('resultado2');
+    atualizar2.textContent = "Valor gasolina: R$ " + resultGasolina.toFixed('2');
+    atualizar2.style.visibility = "visible";
+
+    let atualizar3 = document.getElementById('resultadoFinal');
+    atualizar3.textContent = result;
+    atualizar3.style.visibility = "visible";
+
+    }catch(error){
+        alert(error.message);
+    }
+}
